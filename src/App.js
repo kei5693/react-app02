a30d849b70ff1a0f38be2efcbef859947a72686a
hdk236p <hdk236p@edacom.co.kr>
2022-06-22 17:38:08 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello Wolrd</h1>
+      <FuncComp initNumber={2} />
+      <ClassComp initNumber={2} />
     </div>
   );
+}
+
+function FuncComp(props){
+  var numberState = useState(props.initNumber);
+  var number = numberState[0];
+  var setNumber = numberState[1];
+  return(
+    <div className='container'>
+      <h2>Function Style Component</h2>
+      <p>Number : {number}</p>
+      <input type="button" value="random" onClick={
+          function(){
+            setNumber(Math.random())
+          }
+        } />
+    </div>
+  );
+}
+
+class ClassComp extends React.Component{
+  state = {
+    number: this.props.initNumber
+  }
+  render(){
+    return(
+      <div className='container'>
+        <h2>Class Style Component</h2>
+        <p>Number : {this.state.number}</p>
+        <input type="button" value="random" onClick={
+          function(){
+            this.setState({number: Math.random()})
+          }.bind(this)
+        } />
+      </div>
+    )
+  }
 }
 
 export default App;
