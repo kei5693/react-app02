19b75f0ea7b538c9711160fe1073eeebb5ba8bc3
hdk236p <hdk236p@edacom.co.kr>
2022-06-23 17:49:36 +0900

    0623

--- src/App.js
+++ src/App.js
@@ -30,16 +30,24 @@ function FuncComp(props){
 
 class ClassComp extends React.Component{
   state = {
-    number: this.props.initNumber
+    number: this.props.initNumber,
+    date: (new Date()).toString()
   }
   render(){
     return(
       <div className='container'>
         <h2>Class Style Component</h2>
         <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
+        
         <input type="button" value="random" onClick={
           function(){
             this.setState({number: Math.random()})
+          }.bind(this)
+        } />
+        <input type="button" value="date" onClick={
+          function(){
+            this.setState({date: (new Date()).toString()})
           }.bind(this)
         } />
       </div>
